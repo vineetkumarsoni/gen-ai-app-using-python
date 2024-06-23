3b12569b38ab6a838c8cd0cd4bf7014252ebb410
vineetkumarsoni <106951489+vineetkumarsoni@users.noreply.github.com>
2024-06-23 22:15:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
+
+import streamlit as st
+import os
+
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel('gemini-pro')
+def get_gemini_response(question):
+    response = model.generate_content(question)
+    return response.text
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini Application")
+
+input=st.text_input("Input: ",key="input")
+
+
+submit=st.button("Ask the question")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input)
+    st.subheader("The Response is")
+    st.write(response)
